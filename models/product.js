d84624ea1546848aaef0d6c91cf35e49e5dcec50
joorce@gmail.com <joorce@gmail.com>
2017-12-04 09:11:45 +0100

    Cambios cosméticos

--- models/product.js
+++ models/product.js
@@ -1,20 +1,19 @@
-'use strict';
+'use strict'
 
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
 
 const ProductSchema = mongoose.Schema({
   name: String,
   picture: String,
   price: {
     type: Number,
-    default: 0,
+    default: 0
   },
   category: {
     type: String,
-    enum: ['computer', 'phones', 'accessories'],
+    enum: ['computer', 'phones', 'accessories']
   },
-  description: String,
-});
+  description: String
+})
 
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model('Product', ProductSchema)
